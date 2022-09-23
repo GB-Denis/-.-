8ff72643a3e800df402383defe2a3e7634857389
Denis <dfazylov@mail.ru>
2022-09-24 00:10:56 +0500

    1_Задача_1

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите число 1");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число 2");
 int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"{a} * {a} = {b}");
 if(b == a * a)
 {
