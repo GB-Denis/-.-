07070a3724e3e7d4031917e8dc5ed9d94ee26bac
Denis <dfazylov@mail.ru>
2022-09-24 01:32:16 +0500

    4_Задача

---
+++ Задача_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите положительное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+int num2 = -num;
+while (num2<= num)
+{
+    Console.Write(num2 + ", ");
+    num2 = num2 + 1;
+}
+Console.WriteLine();
