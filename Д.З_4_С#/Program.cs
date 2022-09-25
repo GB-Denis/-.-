0e83ac13480ece7aaa0d137c87141c6fe94eda23
Denis <dfazylov@mail.ru>
2022-09-25 15:22:27 +0500

    4 Задание

---
+++ Д.З_4_С#/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите положительное число от 1 ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+int num2 = num % 2;
+if (num % 2 == 1)
+{
+    num2 = num2 + 1;
+}
+while (num2<= num)
+{
+    Console.Write(num2 + ", ");
+    num2 = num2 + 2;
+}
+Console.WriteLine();
