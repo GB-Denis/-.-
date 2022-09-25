e89e1effb039b3b32f26f670017c8c3d2bf0c59e
Denis <dfazylov@mail.ru>
2022-09-25 14:42:17 +0500

    2 Задание

---
+++ Д.З_2_С#/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b)
+{
+max = b;
+}
+if (max > c)
+{
+    Console.WriteLine("максимальное число: " + max);
+}
+else
+    Console.WriteLine("максимальное число: " + c);
